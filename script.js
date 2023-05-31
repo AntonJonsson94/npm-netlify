1aaf176476048cd2a7ae1faa57a446a062b08399
Anton Jonsson <94antjon@skola.boras.se>
2023-05-31 15:20:59 +0200

    changed function structure

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const card = document.querySelector("#card");
-const input = document.querySelector("#input");
-const button = document.querySelector("#submit");
-const form = document.querySelector("form");
-function printMessage() {
-    card.innerHTML = "";
+const body = document.querySelector("body");
+function printMessage(name) {
+    body.innerHTML = "";
     const message = document.createElement("p");
-    message.innerText = `Hej ${input.value}!`;
-    console.log(input.value);
-    card.append(message);
+    message.innerText = `Hej ${name}!`;
+    console.log(message);
+    body.append(message);
 }
-
-form.addEventListener("submit", () => {
-    printMessage();
-});
