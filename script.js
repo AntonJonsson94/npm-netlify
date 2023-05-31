9e73d805f2889e1f0fb48314e8139cdac1e86ac8
Anton Jonsson <94antjon@skola.boras.se>
2023-05-31 14:29:05 +0200

    added form submit and a required property for the input

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const card = document.querySelector("#card");
 const input = document.querySelector("#input");
 const button = document.querySelector("#submit");
-
+const form = document.querySelector("form");
 function printMessage() {
     card.innerHTML = "";
     const message = document.createElement("p");
     message.innerText = `Hej ${input.value}!`;
-
+    console.log(input.value);
     card.append(message);
 }
 
-button.addEventListener("click", () => {
+form.addEventListener("submit", () => {
     printMessage();
 });
